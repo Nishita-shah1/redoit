402d230029ded9260cc326e254e9bbc9fefb8916
nishita-shah1 <nishita182005@gmail.com>
2025-11-19 20:08:05 +0530

    added suspense to remove error

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Footer } from "./_components/Footer";
 import { Heading } from "./_components/Heading";
 import { Heroes } from "./_components/Heroes";
@@ -5,11 +6,16 @@ import { FeaturesGrid } from "./_components/FeaturesGrid";
 import { Comparision } from "./_components/Comparison";
 import { Pricing } from "./_components/Pricing";
 
+import { Suspense } from "react";
+
 export default function LandingPage() {
   return (
     <div className="flex min-h-full flex-col dark:bg-[#1F1F1F]">
       <div className="flex flex-1 flex-col items-center justify-center gap-y-8 px-6 pb-10 text-center md:justify-start">
-        <Heading />
+        {/* WRAP HEADING IN SUSPENSE */}
+        <Suspense fallback={<div className="h-full w-full bg-background" />}>
+           <Heading />
+        </Suspense>
         <Heroes />
         <FeaturesGrid />
         <Comparision />
